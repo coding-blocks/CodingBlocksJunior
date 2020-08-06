d6b4bde947bb4332319705373eae600c37676603
Jatin Katyal <jatin.katyal13@gmail.com>
2020-08-06 17:09:50 +0530

    setup router and views

---
+++ lib/ui/views/courses/courses_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+import 'courses_viewmodel.dart';
+
+class CoursesView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<CoursesViewModel>.reactive(
+      builder: (context, model, child) => Scaffold(
+        body: Center(
+          child: Text('Hello World')
+        ),
+      ), 
+      viewModelBuilder: () => CoursesViewModel(),
+    );
+  }
+}
