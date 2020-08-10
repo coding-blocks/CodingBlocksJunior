f7cf38b8073f41531b119c2b3b3401bb7e0e74b2
Jatin Katyal <jatin.katyal13@gmail.com>
2020-08-11 01:57:01 +0530

    derp

--- lib/app/router.gr.dart
+++ lib/app/router.gr.dart
@@ -9,28 +9,24 @@
 import 'package:auto_route/auto_route.dart';
 
 import '../ui/views/course/view.dart';
-import '../ui/views/dashboard/bottom_bar_pages/bookmarks/view.dart';
-import '../ui/views/dashboard/bottom_bar_pages/home/view.dart';
-import '../ui/views/dashboard/bottom_bar_pages/notes/view.dart';
-import '../ui/views/dashboard/bottom_bar_pages/profile/view.dart';
 import '../ui/views/dashboard/view.dart';
 import '../ui/views/on_board/view.dart';
 import '../ui/views/player/view.dart';
 
 class Routes {
-  static const String onBoardView = '/';
+  static const String onBoardView = '/on-board-view';
   static const String _courseView = '/courses/:slug';
   static String courseView({@required dynamic slug}) => '/courses/$slug';
+  static const String dashboardView = '/';
   static const String _playerView = '/courses/:courseId/contents/:contentId';
   static String playerView(
           {@required dynamic courseId, @required dynamic contentId}) =>
       '/courses/$courseId/contents/$contentId';
-  static const String dashboardView = '/dashboard';
   static const all = <String>{
     onBoardView,
     _courseView,
-    _playerView,
     dashboardView,
+    _playerView,
   };
 }
 
@@ -40,12 +36,8 @@ class Router extends RouterBase {
   final _routes = <RouteDef>[
     RouteDef(Routes.onBoardView, page: OnBoardView),
     RouteDef(Routes._courseView, page: CourseView),
+    RouteDef(Routes.dashboardView, page: DashboardView),
     RouteDef(Routes._playerView, page: PlayerView),
-    RouteDef(
-      Routes.dashboardView,
-      page: DashboardView,
-      generator: DashboardViewRouter(),
-    ),
   ];
   @override
   Map<Type, AutoRouteFactory> get pagesMap => _pagesMap;
@@ -62,69 +54,15 @@ class Router extends RouterBase {
         settings: data,
       );
     },
-    PlayerView: (data) {
-      return buildAdaptivePageRoute<dynamic>(
-        builder: (context) => PlayerView(),
-        settings: data,
-      );
-    },
     DashboardView: (data) {
       return buildAdaptivePageRoute<dynamic>(
         builder: (context) => DashboardView(),
         settings: data,
       );
     },
-  };
-}
-
-class DashboardViewRoutes {
-  static const String dashboardHomeView = '/';
-  static const String dashboardBookmarksView = '/dashboard-bookmarks-view';
-  static const String dashboardNotesView = '/dashboard-notes-view';
-  static const String dashboardProfileView = '/dashboard-profile-view';
-  static const all = <String>{
-    dashboardHomeView,
-    dashboardBookmarksView,
-    dashboardNotesView,
-    dashboardProfileView,
-  };
-}
-
-class DashboardViewRouter extends RouterBase {
-  @override
-  List<RouteDef> get routes => _routes;
-  final _routes = <RouteDef>[
-    RouteDef(DashboardViewRoutes.dashboardHomeView, page: DashboardHomeView),
-    RouteDef(DashboardViewRoutes.dashboardBookmarksView,
-        page: DashboardBookmarksView),
-    RouteDef(DashboardViewRoutes.dashboardNotesView, page: DashboardNotesView),
-    RouteDef(DashboardViewRoutes.dashboardProfileView,
-        page: DashboardProfileView),
-  ];
-  @override
-  Map<Type, AutoRouteFactory> get pagesMap => _pagesMap;
-  final _pagesMap = <Type, AutoRouteFactory>{
-    DashboardHomeView: (data) {
+    PlayerView: (data) {
       return buildAdaptivePageRoute<dynamic>(
-        builder: (context) => DashboardHomeView(),
-        settings: data,
-      );
-    },
-    DashboardBookmarksView: (data) {
-      return buildAdaptivePageRoute<dynamic>(
-        builder: (context) => DashboardBookmarksView(),
-        settings: data,
-      );
-    },
-    DashboardNotesView: (data) {
-      return buildAdaptivePageRoute<dynamic>(
-        builder: (context) => DashboardNotesView(),
-        settings: data,
-      );
-    },
-    DashboardProfileView: (data) {
-      return buildAdaptivePageRoute<dynamic>(
-        builder: (context) => DashboardProfileView(),
+        builder: (context) => PlayerView(),
         settings: data,
       );
     },
