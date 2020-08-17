983b660658cd599e68e1c810fca15706e78ba417
Jatin Katyal <jatin.katyal13@gmail.com>
2020-08-17 21:39:27 +0530

    derp #2

--- lib/ui/views/player/viewmodel.dart
+++ lib/ui/views/player/viewmodel.dart
@@ -22,11 +22,7 @@ class PlayerViewModel extends FutureViewModel<Content> {
   Future<Content> futureToRun() async {
     content = await fetchContent();
     setupYoutubePlayer(content.url);
-<<<<<<< Updated upstream
 //    updateProgress();
-=======
-    // updateProgress(); 
->>>>>>> Stashed changes
     course = await fetchCourse();
     return content;
   }
