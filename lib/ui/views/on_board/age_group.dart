490cccf49bee288cace892a6a673f32659970f41
Jatin Katyal <jatin.katyal13@gmail.com>
2020-08-29 15:33:38 +0530

    fix onboarding class list item

--- lib/ui/views/on_board/age_group.dart
+++ lib/ui/views/on_board/age_group.dart
@@ -11,16 +11,28 @@ class OnboardAgeGroup extends StatefulWidget {
 }
 
 class OnboardAgeGroupState extends State<OnboardAgeGroup> {
-  List<RadioModel> sampleData = new List<RadioModel>();
-
-  @override
-  void initState() {
-    super.initState();
-    sampleData.add(new RadioModel(false, 'Foundation', ["1", "2" "3"]));
-    sampleData.add(new RadioModel(false, 'Junior', ["4", "5", "6"]));
-    sampleData.add(new RadioModel(false, 'Middle', ["7", "8", "9", "10"]));
-    sampleData.add(new RadioModel(false, 'Senior', ["11", "12"]));
-  }
+  List<RadioModel> sampleData = [
+    RadioModel(false, "Foundation", [
+      ClassPillModel(text: "1", color: getColorFromHex("#F5C792")),
+      ClassPillModel(text: "1", color: getColorFromHex("#85CEF2")),
+      ClassPillModel(text: "1", color: getColorFromHex("#E99C9F"))
+    ]),
+    RadioModel(false, "Junior", [
+      ClassPillModel(text: "1", color: getColorFromHex("#F5C792")),
+      ClassPillModel(text: "1", color: getColorFromHex("#85CEF2")),
+      ClassPillModel(text: "1", color: getColorFromHex("#E99C9F"))
+    ]),
+    RadioModel(false, "Middle", [
+      ClassPillModel(text: "1", color: getColorFromHex("#F5C792")),
+      ClassPillModel(text: "1", color: getColorFromHex("#85CEF2")),
+      ClassPillModel(text: "1", color: getColorFromHex("#E99C9F"))
+    ]),
+    RadioModel(false, "Senior", [
+      ClassPillModel(text: "1", color: getColorFromHex("#F5C792")),
+      ClassPillModel(text: "1", color: getColorFromHex("#85CEF2")),
+      ClassPillModel(text: "1", color: getColorFromHex("#E99C9F"))
+    ]),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -99,16 +111,22 @@ class RadioItem extends StatelessWidget {
           ),
           Expanded(
             flex: 1,
-            child: new ListView.builder(
-              shrinkWrap: true, //Added
-              itemCount: 1,
-              itemBuilder: (BuildContext context, int index) {
-                return new Text('abc');
-              },
+            child: Container(
+              padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+              child: Row(
+                children: _item.classes.map((classPill) => 
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                    child: ClassPill(
+                      klass: classPill.text,
+                      color: classPill.color,
+                    ),
+                  )
+                ).toList(),
+              ),
             ),
           ),
-          Spacer(),
-          new Visibility(
+          Visibility(
             maintainSize: true,
             maintainAnimation: true,
             maintainState: true,
@@ -124,10 +142,42 @@ class RadioItem extends StatelessWidget {
   }
 }
 
+class ClassPill extends StatelessWidget {
+  final String klass;
+  final Color color;
+
+  ClassPill({ 
+    @required this.klass,
+    this.color
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5),
+        color: color,
+      ),
+      padding: EdgeInsets.fromLTRB(8, 4, 8, 4),
+      child: Text(klass),
+    );
+  }
+}
+
 class RadioModel {
   bool isSelected;
   final String text;
-  final List<String> classes;
+  final List<ClassPillModel> classes;
 
   RadioModel(this.isSelected, this.text, this.classes);
+}
+
+class ClassPillModel {
+  final String text;
+  final Color color;
+
+  ClassPillModel({
+    @required this.text,
+    this.color
+  });
 }
