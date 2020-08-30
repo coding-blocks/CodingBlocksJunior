00fef0f21e9fc98790c7ba15be908d80677ad4c8
Jatin Katyal <jatin.katyal13@gmail.com>
2020-08-30 23:50:25 +0530

    fetch only user notes and bookmarks

--- lib/ui/views/dashboard/bottom_bar_pages/bookmarks/viewmodel.dart
+++ lib/ui/views/dashboard/bottom_bar_pages/bookmarks/viewmodel.dart
@@ -1,14 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:coding_blocks_junior/app/locator.dart';
 import 'package:coding_blocks_junior/models/bookmark.dart';
+import 'package:coding_blocks_junior/services/session.dart';
 import 'package:stacked/stacked.dart';
 
 class DashboardBookmarksViewModel extends StreamViewModel<QuerySnapshot> {
+  SessionService sessionService = locator<SessionService>();
   List<Bookmark> bookmarks = [];
 
   @override
   Stream<QuerySnapshot> get stream => Firestore
     .instance
     .collection('bookmarks')
+    .where("userId", isEqualTo: sessionService.user.uid)
     .snapshots();
 
   @override
